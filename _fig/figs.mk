52b273d8a2b447ef90f308b55b07bb8bfbb6dba8
Paul Batchelor <thisispaulbatchelor@gmail.com>
2020-09-09 10:58:32 -0400

    figures for peakeq

--- _fig/figs.mk
+++ _fig/figs.mk
@@ -7,4 +7,9 @@ chaosnoise_gamma.png \
 osc_lerp.png \
 osc_lerp_simp.png \
 osc_log2.png \
+peakeq_compute_gain.png \
+peakeq_df1_difference.png \
+peakeq_df2_difference.png \
+peakeq_df2_structure.png \
+peakeq_tforiginal.png \
 
