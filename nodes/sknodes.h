3ec4dead8c6d4b2792384e31b91ae076a1b99d29
Paul Batchelor <thisispaulbatchelor@gmail.com>
2021-03-21 17:37:52 -0400

    added functions to sknodes.h

--- nodes/sknodes.h
+++ nodes/sknodes.h
@@ -24,4 +24,7 @@ int sk_node_expon(sk_core *core);
 int sk_node_rline(sk_core *core);
 int sk_node_vardelay(sk_core *core);
 int sk_node_expmap(sk_core *core);
+int sk_node_biramp(sk_core *core);
+int sk_node_phasewarp(sk_core *core);
+int sk_node_oscf(sk_core *core);
 #endif
