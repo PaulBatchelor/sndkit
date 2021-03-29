6c10110bf894f05e11f8ba4c11aabb39092efc2f
Paul Batchelor <thisispaulbatchelor@gmail.com>
2021-03-29 16:39:24 -0400

    added sklil_main

--- nodes/sklil.c
+++ nodes/sklil.c
@@ -6,6 +6,11 @@
 #include "core.h"
 #include "sklil.h"
 
+int sklil_main(int argc, char *argv[])
+{
+    return lil_main(argc, argv, sklil_loader, sklil_clean);
+}
+
 int sklil_param(sk_core *core, lil_value_t arg)
 {
     const char *sarg;
