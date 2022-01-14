ebde39d8cf115dc4a0123fece57b336c3ae56d85
Paul Batchelor <thisispaulbatchelor@gmail.com>
2022-01-14 10:11:07 -0500

    update arity check for butterworth filters

--- nodes/butterworth/l_butterworth.c
+++ nodes/butterworth/l_butterworth.c
@@ -15,7 +15,7 @@ static lil_value_t butlp(lil_t lil, size_t argc, lil_value_t *argv)
     int rc;
     core = lil_get_data(lil);
 
-    SKLIL_ARITY_CHECK(lil, "butlp", argc, 1);
+    SKLIL_ARITY_CHECK(lil, "butlp", argc, 2);
 
     rc = sklil_param(core, argv[0]);
     SKLIL_PARAM_CHECK(lil, rc, "butlp");
@@ -33,7 +33,7 @@ static lil_value_t buthp(lil_t lil, size_t argc, lil_value_t *argv)
     int rc;
     core = lil_get_data(lil);
 
-    SKLIL_ARITY_CHECK(lil, "buthp", argc, 1);
+    SKLIL_ARITY_CHECK(lil, "buthp", argc, 2);
 
     rc = sklil_param(core, argv[0]);
     SKLIL_PARAM_CHECK(lil, rc, "buthp");
